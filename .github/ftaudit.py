26145550ddf59837e669faa249cf1a550c39769f
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2023-07-19 01:29:15 -0300

    Ran autoformatter

--- .github/ftaudit.py
+++ .github/ftaudit.py
@@ -1,47 +1,49 @@
-import json
-import sys
-
-
-import shutil
-import os.path as path
+from enum import Enum
+from itertools import chain
 from os import walk
-
 from typing import NamedTuple, Optional, TypeVar, Union, Generic, cast
 
 import filetype as ft
-from enum import Enum
-
-from itertools import chain
+import json
+import os.path as path
+import shutil
+import sys
 
 
 OkT, ErrT = TypeVar("R"), TypeVar("E")
 
-class Result(Generic[OkT, ErrT]): 
+
+class Result(Generic[OkT, ErrT]):
     def unwrap(self) -> OkT:
         if self.ok is True:
             return cast(OkT, self.value)
 
         raise ValueError("Attempted to unwrap an Err value.")
 
-    def __init__(self, value: Union[OkT, ErrT], ok: bool): 
+    def __init__(self, value: Union[OkT, ErrT], ok: bool):
         self.value = value
         self.ok = ok
 
-class Ok(Generic[OkT, ErrT], Result[OkT, ErrT]): 
-    def __init__(self, value: OkT): super().__init__(value, True)
 
-class Err(Generic[OkT, ErrT], Result[OkT, ErrT]): 
-    def __init__(self, value: ErrT): super().__init__(value, False)
+class Ok(Generic[OkT, ErrT], Result[OkT, ErrT]):
+    def __init__(self, value: OkT):
+        super().__init__(value, True)
 
 
-Discrepancy = NamedTuple("T", [
-    ("file_path", str), ("incorrect", str), ("correct", str)])
+class Err(Generic[OkT, ErrT], Result[OkT, ErrT]):
+    def __init__(self, value: ErrT):
+        super().__init__(value, False)
+
+
+Discrepancy = NamedTuple(
+    "T", [("file_path", str), ("incorrect", str), ("correct", str)]
+)
 
 
 def examine_file(file_path: str) -> Result[Optional[Discrepancy], str]:
     if not path.isfile(file_path):
         return Err(f'The path "{file_path}" is not a valid file.')
-    
+
     file_type = ft.guess(file_path)
 
     if file_type is None:
@@ -50,40 +52,43 @@ def examine_file(file_path: str) -> Result[Optional[Discrepancy], str]:
     file_extension: str = path.splitext(file_path)[1].lower()[1:]
 
     # Standardize multiple accepted extensions down to a single extension, the
-    # one used by the filetype library, as it does not recognize alternative
+    # one used by the file type library, as it does not recognize alternative
     # extensions, only one extension per file type.
 
-    file_extension = {
-        "jpeg": "jpg",
-        "tiff": "tif"
-    }.get(file_extension, file_extension)
+    file_extension = {"jpeg": "jpg", "tiff": "tif"}.get(
+        file_extension, file_extension
+    )
 
     # Mismatch between the expected extension for the determined file type,
     # and the actual extension of the file.
 
     if file_type.extension != file_extension:
-        return Ok(Discrepancy(
-            file_path = file_path, 
-            incorrect = file_extension, 
-            correct   = file_type.extension
-        ))
+        return Ok(
+            Discrepancy(
+                file_path=file_path,
+                incorrect=file_extension,
+                correct=file_type.extension,
+            )
+        )
 
     # No discrepancy.
     return Ok(None)
 
 
 def generate_csv_report(
-    discrepancies: list[Discrepancy], 
-    delimiter = ','
+    discrepancies: list[Discrepancy], delimiter=","
 ) -> Result[list[str], str]:
-
     if len(discrepancies) == 0:
         return Err("No discrepancies to report.")
-    
+
     try:
-        report: list[str] = [f"IncorrectExtension{delimiter}CorrectExtension{delimiter}FilePath", *[
-            f'{d.incorrect}{delimiter}{d.correct}{delimiter}{d.file_path}' for d in discrepancies 
-        ]]
+        report: list[str] = [
+            f"IncorrectExtension{delimiter}CorrectExtension{delimiter}FilePath",
+            *[
+                f"{d.incorrect}{delimiter}{d.correct}{delimiter}{d.file_path}"
+                for d in discrepancies
+            ],
+        ]
     except Exception as e:
         return Err(f"Encountered an exception while generating CSV report: {e}")
 
@@ -91,33 +96,31 @@ def generate_csv_report(
 
 
 def generate_json_report(
-    discrepancies: list[Discrepancy]
+    discrepancies: list[Discrepancy],
 ) -> Result[list[dict], str]:
-
     if len(discrepancies) == 0:
         return Err("No discrepancies to report.")
 
     try:
         report: list[dict] = [
-            { "file_path": d.file_path,
-              "incorrect": d.incorrect,
-              "correct":   d.correct
-            } for d in discrepancies
+            {
+                "file_path": d.file_path,
+                "incorrect": d.incorrect,
+                "correct": d.correct,
+            }
+            for d in discrepancies
         ]
     except Exception as e:
-        return Err(f"Encountered an exception while generating JSON report: {e}")
-    
+        return Err(
+            f"Encountered an exception while generating JSON report: {e}"
+        )
 
     return Ok(report)
 
 
-
-
 def fix_discrepancies(
-    discrepancies: list[Discrepancy],
-    what_if: bool = True
+    discrepancies: list[Discrepancy], what_if: bool = True
 ) -> Result[None, str]:
-
     def fix_discrepancy(discrepancy: Discrepancy) -> Result[None, str]:
         try:
             wrong_abs: str = path.abspath(discrepancy.file_path)
@@ -125,7 +128,9 @@ def fix_discrepancies(
             wrong_spl: tuple[str, str] = path.split(wrong_abs)
             wrong_nex: str = path.splitext(wrong_bas)[0]
 
-            correct_abs: str = path.join(wrong_spl[0], f'{wrong_nex}.{discrepancy.correct}')
+            correct_abs: str = path.join(
+                wrong_spl[0], f"{wrong_nex}.{discrepancy.correct}"
+            )
 
             if what_if:
                 print(f'Would move "{wrong_abs}" to "{correct_abs}"')
@@ -133,27 +138,35 @@ def fix_discrepancies(
                 try:
                     shutil.move(wrong_abs, correct_abs)
                 except Exception as e:
-                    return Err(f'Encountered an exception while fixing discrepancy "{discrepancy.file_path}" -> "{discrepancy.correct}": {e}')
+                    return Err(
+                        f'Encountered an exception while fixing discrepancy "{discrepancy.file_path}" -> "{discrepancy.correct}": {e}'
+                    )
         except OSError as e:
-            return Err(f"Encountered an exception while fixing discrepancy: {e}")
+            return Err(
+                f"Encountered an exception while fixing discrepancy: {e}"
+            )
 
         return Ok(None)
 
     for d in discrepancies:
         fix_discrepancy(d)
-    
+
     # r = map(fix_discrepancy, discrepancies)
     return Ok(None)
 
+
 # If this isn't faster than glob, I'm going to be very disappointed.
 def gather_file_paths(start_path: str) -> Result[list[str], str]:
-    return Ok([
-        *chain.from_iterable(
-            map(lambda x: 
-                map(lambda y: 
-                    path.join(x[0], y), x[2]), walk(start_path))
-        )
-    ])
+    return Ok(
+        [
+            *chain.from_iterable(
+                map(
+                    lambda x: map(lambda y: path.join(x[0], y), x[2]),
+                    walk(start_path),
+                )
+            )
+        ]
+    )
 
 
 HELP: str = """
@@ -185,7 +198,7 @@ Modifying Operations:
                         actually modify any files. Not set by default.
 """
 
-OutputFormat = Enum("OutputFormat", ('Text', 'Json', 'CSV'))
+OutputFormat = Enum("OutputFormat", ("Text", "Json", "CSV"))
 
 if __name__ == "__main__":
     search_start: str = "."
@@ -199,7 +212,7 @@ if __name__ == "__main__":
         next_argument: str | None = (
             sys.argv[index + 1] if index + 1 < len(sys.argv) else None
         )
-        
+
         if argument in ("--help", "-h"):
             print(HELP)
             exit(0)
@@ -213,7 +226,7 @@ if __name__ == "__main__":
         elif argument in ("--fix", "-f"):
             fixing_enabled = True
 
-        elif argument in ('--what-if', '-w'):
+        elif argument in ("--what-if", "-w"):
             what_if = True
 
         if not isinstance(next_argument, str):
@@ -225,15 +238,19 @@ if __name__ == "__main__":
         elif argument in ("--output", "-o"):
             output_file = next_argument
 
-        elif argument in ('--indent', '-i'):
+        elif argument in ("--indent", "-i"):
             try:
                 json_indent = int(next_argument)
             except ValueError as e:
-                print(f'Invalid indent value: "{next_argument}"; must be convertible to an integer.')
-                print(f'Exception: {e}')
+                print(
+                    f'Invalid indent value: "{next_argument}"; must be convertible to an integer.'
+                )
+                print(f"Exception: {e}")
                 exit(1)
             except Exception as e:
-                print(f'Unexpected exception when converting "{next_argument}" to an integer: {e}')
+                print(
+                    f'Unexpected exception when converting "{next_argument}" to an integer: {e}'
+                )
 
     r_file_list: Result[list[str], str] = gather_file_paths(search_start)
 
@@ -242,30 +259,33 @@ if __name__ == "__main__":
         exit(1)
 
     file_list: list[str] = r_file_list.unwrap()
-        
 
     map(lambda fp: examine_file(fp), file_list)
-
 
     results = map(lambda fp: examine_file(fp), file_list)
 
     discrepant_results: list[Discrepancy] = [
-        *map(Result.unwrap, 
-            filter(lambda r: r.ok and r.unwrap() is not None, results))
+        *map(
+            Result.unwrap,
+            filter(lambda r: r.ok and r.unwrap() is not None, results),
+        )
     ]
 
     output: Optional[str] = None
-    
+
     if output_format == OutputFormat.Text:
         output = "\n".join(
-            [f'{r.incorrect} should be {r.correct} for file {r.file_path}' 
-                for r in discrepant_results])
+            [
+                f"{r.incorrect} should be {r.correct} for file {r.file_path}"
+                for r in discrepant_results
+            ]
+        )
 
     elif output_format == OutputFormat.Json:
         json_report = generate_json_report(discrepant_results)
 
         if json_report.ok:
-            output = json.dumps(json_report.unwrap(), indent = json_indent)
+            output = json.dumps(json_report.unwrap(), indent=json_indent)
         else:
             print(json_report.value)
             exit(1)
@@ -280,23 +300,26 @@ if __name__ == "__main__":
             exit(1)
 
     if output is None:
-        print(f'No output was generated for format {output_format}. This could be a bug.')
+        print(
+            f"No output was generated for format {output_format}. This could be a bug."
+        )
         exit(1)
 
     print(output)
 
     try:
         if isinstance(output_file, str):
-            with open(output_file, 'w+') as io:
+            with open(output_file, "w+") as io:
                 io.write(output)
     except Exception as e:
-        print(f'Encountered an exception while writing to output file {output_file}: {e}')
+        print(
+            f"Encountered an exception while writing to output file {output_file}: {e}"
+        )
         exit(1)
 
     # This should be the very last operation, since it has the ability to modify
-    # file names. Keeping it last ensures that any errors will have occured before
+    # file names. Keeping it last ensures that any errors will have occurred before
     # this point, minimizing risk for the uncertain (I miss Rust).
 
     if fixing_enabled:
         fix_discrepancies(discrepant_results, what_if)
-
